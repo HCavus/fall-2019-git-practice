cb580b764bc719d9fb55952caab15d28fa46581a
HCavus <58565813+HCavus@users.noreply.github.com>
2020-03-18 20:04:48 -0400

    Update FirstClass.java

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -1,5 +1,7 @@
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("Brave New World");
+        System.out.println("someone");
+         System.out.println("change one");
     }
 }
